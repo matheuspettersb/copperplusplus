0d933b69d9487c399a15d603cf3e4627d4b3cfa6
Matheus Petters <teteubevilaqua@hotmail.com>
2024-09-29 22:57:31 -0300

    Initial commit

---
+++ src/main/java/lakao/copperpp/CopperPPClient.java
@@ -0,0 +1,11 @@
+package lakao.copperpp;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CopperPPClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
